4bc82d766664e0826908443b390749f88d7b4731
Juliana Neto <juneto.s88@gmail.com>
2026-01-09 06:34:10 -0300

    fix typo

--- src/main/java/bootcamp/Main.java
+++ src/main/java/bootcamp/Main.java
@@ -1,4 +1,4 @@
-package dobackaofront;
+package bootcamp;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -6,11 +6,11 @@ public class Main {
     public static void main(String[] args) {
         // modificadores de acesso - public, protected, private[
         /*
-            VARIAVEIS - tipos de dados (intm float, boolean, char, String
-            ESTRUTURAS CONDICIONAIS - if, else, else if, switch
-            ESTRUTURAS DE REPETIÇÃO - for, while, do while
+            VARIAVEIS - tipos de dados (intm float, boolean, char, String = JA ENTENDO
+            ESTRUTURAS CONDICIONAIS - if, else, else if, switch = JA ENTENDO
+            ESTRUTURAS DE REPETIÇÃO - for, while, do while = JA ENTENDO
             BLOCOS DE CODIGOS(metodos)
-            ORIENTAÇÃO A OBJETOS POO
+            ORIENTAÇÃO A OBJETOS POO -- FOCO NISSO
                 -classes
                 -objetos
                 -atributos
@@ -20,11 +20,11 @@ public class Main {
                 -polimorfismo
                 -associação
                 -interface
-                -classes e métodos abstratos(absttração)
+                -classes e métodos abstratos(abstração)
                 -coleções
                 -pacotes
-       System.out.println("Hello World");
 
          */
+        System.out.println("Hello World");
     }
 }
